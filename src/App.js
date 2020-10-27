6987e3c4a31b09e600e298b3e5af546b06803111
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-10-27 20:20:03 +0530

    Completed Covid API web app.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import { Cards, CountryPicker, Chart } from "./components";
 import styles from "./app.module.css";
 import { fetchData } from "./api";
+import covidImage from "./images/covid19.png";
 
 const App = () => {
   const [data, setData] = useState("");
@@ -32,6 +33,7 @@ const App = () => {
 
   return (
     <div className={styles.container}>
+      <img className={styles.image} src={covidImage} alt="Covid-19" />
       <Cards data={data} />
       <CountryPicker handleChange={handleChange} />
       <Chart country={country} data={data} />
